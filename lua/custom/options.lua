c45ace83f9f91305cb8271d6f5ece107f419e3af
imAsparky <mark.sevelj@dunwright.com.au>
2024-07-05 09:35:01 +0800

    chore(plugin): Add and configure Obsidian #21
    closes #21

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -9,3 +9,6 @@ vim.opt.foldcolumn = '1' -- Using 0 is not bad
 vim.opt.foldenable = true
 vim.opt.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.opt.foldlevelstart = 99 -- keeps folds open when changing buffers
+
+-- setup conceallevel to enable it in obsidian.nvim
+vim.opt.conceallevel = 1
--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -23,7 +23,50 @@ return {
         path = '/mnt/g/My Drive/_Dunwright/Obsidian/Dunwright-Dev/',
       },
     },
-  },
+    completion = {
+      -- Set to false to disable completion.
+      nvim_cmp = true,
+      -- Trigger completion at 2 chars.
+      min_chars = 2,
+    },
+    mappings = {
+      -- Obsidian follow
+      ['<leader>og'] = {
+        action = function()
+          return require('obsidian').util.gf_passthrough()
+        end,
+        opts = {
+          noremap = false,
+          expr = true,
+          buffer = true,
+        },
+      },
+      -- Toggle check-boxes "obsidian done"
+      ['<leader>od'] = {
+        action = function()
+          return require('obsidian').util.toggle_checkbox()
+        end,
+        opts = { buffer = true },
+      },
+    },
+    note_frontmatter_func = function(note)
+      -- This is the equivalent to the default frontmatter function, with the area and project added.
+      local out = { id = note.id, aliases = note.aliases, tags = note.tags, area = '', project = '' }
+      -- 'note.metadata' containd any manual added fields in the frontmatter.
+      --  Seo here we just make sure those fieldss are kept in the frontmatter.
+      if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+        for k, v in pairs(note.metadata) do
+          out[k] = v
+        end
+      end
+      return out
+    end,
 
-  -- see below for full list of options 👇
+    templates = {
+      subdir = 'Templates',
+      date_format = '%d-%m-%Y-%a',
+      time_format = '%H:%M',
+      tags = '',
+    },
+  },
 }
